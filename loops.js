884660c1dc0004d45eac1b332627c61d2d12599f
Ron Clark <ronaldtclark@gmail.com>
2018-06-13 01:46:33 -0500

    Commit 9 - Created loops.js file

---
+++ loops.js
@@ -0,0 +1,27 @@
+for (i=0; i<100; i++) {
+  console.log (i);
+}
+
+for (i=99; i>=0; i--) {
+  console.log (i);
+}
+
+for (i=0; i<100; i++) {
+  if (i%2===0) {console.log (i)};
+}
+
+for (i=98; i>=0; i--) {
+  if (i%2===0) {console.log (i)};
+}
+
+for (i=49; i<=72; i++) {
+  console.log (i);
+}
+
+for (i=81; i>=26; i--) {
+  console.log (i);
+}
+
+for (i=3; i<=90; i++) {
+  if (i%3===0) {console.log (i)};
+}
